37c3956b002b2a6fa8dd184c3251f381156b3e41
Daniil <46220468+daangrah@users.noreply.github.com>
2025-03-25 19:06:30 +0300

    todo is ready for interview

--- src/components/DeleteTask.jsx
+++
@@ -1,7 +0,0 @@
-export const DeleteTask = ({deleteTask, task}) => {
-    return (
-        <button className="delete-btn" onClick={()=> deleteTask(task)}>
-            ❌
-        </button>
-    )
-}
--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-import {createStore} from "redux";
-import {taskReducer} from "./TaskReducer";
-import {composeWithDevTools} from "redux-devtools-extension";
-
-export const store = createStore(taskReducer, composeWithDevTools())
